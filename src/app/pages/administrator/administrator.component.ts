136715b7829f5451593658d9b91d5472dae89746
Nicolas Ferretti <nicolas_ferretti@mcafee.com>
2018-04-07 23:26:52 -0300

    Menu administrador

--- src/app/pages/administrator/administrator.component.ts
+++ src/app/pages/administrator/administrator.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CategoryService} from '../../services/category/category.service';
-import {AuthenticationService} from '../../services/login/login.service';
+declare let jquery: any;
+declare let $: any;
 
 @Component({
   selector: 'app-administrator',
@@ -11,10 +12,21 @@ export class AdministratorComponent implements OnInit {
 
   public categoryList = [];
   public errorWithService: any;
+  category: any = {};
   constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
     this.showCategories();
+  }
+
+  newCategory() {
+    this.categoryService.newCategory(this.category).subscribe(
+      category => {
+        this.showCategories();
+      },
+      error => this.errorWithService = error
+    );
+    this.clearCategoryForm();
   }
 
   showCategories() {
@@ -34,4 +46,8 @@ export class AdministratorComponent implements OnInit {
       );
   }
 
+  clearCategoryForm() {
+    this.category.name = null;
+    $('.nav-tabs a[href="#showCategories"]').tab('show');
+  }
 }
